b9853252ae32581b4c18a29720ec7c2387ed5660
Kai Cheung <kaicheunga@gmail.com>
2022-08-03 01:12:50 +0100

    Resolved tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, elem1, elem2) {
+    const args = [...arguments]
+    
+    for (let i = 1; i < arguments.length; i++) {
+        if (arr.includes(args[i])) {
+            arr.splice(arr.indexOf(args[i]),1)
+        }
+    }
+    
+    return arr
 };
 
 // Do not edit below this line
